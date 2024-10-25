56dd56cfdb863373e8c3467d72820e12aa63de6e
Tisucute <ngocanhthugialai@gmail.com>
2024-10-25 10:23:54 +0700

    update nhan vien, tai khoan, vi tri, khach hang

--- Project/Program.cs
+++ Project/Program.cs
@@ -16,7 +16,7 @@ namespace Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_ViTriCongViec());
+            Application.Run(new frm_TaiKhoan());
         }
     }
 }
